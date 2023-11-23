7b9d694554dc785faef400e80da41b10608417cc
Gadevelop <gadsean4ira@gmail.com>
2023-11-23 23:50:49 +0200

    Initial commit

---
+++ variables/variable_walking.py
@@ -0,0 +1,16 @@
+# Declaring Variable
+
+language = "Python"
+print(language)
+
+# Re-declaring variable
+language = "Go-Lang"
+print(language)
+
+# Multiple assignment
+# Assigning a single value to multiple variables
+new_year = bad_year = 2022
+favorite_team = epl_team = "Liverpool"
+print(new_year, bad_year)
+print(favorite_team)
+print(epl_team)
